bd3e97a2b9a4b278db11b74404adb0575451bf8f
MarcoBrunoBR <marco.bruno.br@gmail.com>
2018-06-19 21:13:21 -0300

    Explicação do EJS

--- casadocodigo/app.js
+++ casadocodigo/app.js
@@ -3,17 +3,19 @@ const port = 3001
 
 const server = express()
 
+server.set('view engine', 'ejs')
+
 server.use((req, res, next) => {
   console.log(req.url)
   next()
 })
-
+// JSON -> JavaScript Object Notation
 server.get('/', (req, res) => {
-  res.send('<h1>Home</h1>')
+  res.render('index')
 })
 
 server.get('/contato', (req, res) => {
-  res.send('<h1>Contato</h1>')
+  res.render('contato')
 })
 
 server.use((req, res) => {
